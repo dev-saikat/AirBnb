e282a1185857df0604a4380b78595df3d313efe5
saikatberabca <saikat9382@gmail.com>
2025-01-18 23:52:55 +0530

    add redirect handling and user session management to login/logout flows

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware");
 
 
 //SignUp
@@ -15,8 +16,13 @@ router.post("/signup", wrapAsync(async (req, res) => {
         let { username, email, password } = req.body;
         const newUser = new User({ email, username });
         const registerUser = await User.register(newUser, password);
-        req.flash("success", "User Was Registered");
-        res.redirect("/listings");
+        req.login(registerUser,(err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "User Was Registered");
+            res.redirect("/listings");
+        })
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -29,10 +35,23 @@ router.get("/login", (req, res) => {
     res.render("users/signin.ejs");
 });
 
-router.post("/login", passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
+router.post("/login",saveRedirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
     req.flash("success", "Welcome back to AirBnb You are Successfully Loged in");
-    res.redirect("/listings");
+
+    res.redirect(res.locals.redirectUrl);
     
-})
+});
+
+//Log Out
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "You are logged out!");
+        res.redirect("/listings");
+    });
+});
 
 module.exports = router;
